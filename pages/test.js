e3bc27127a8815d6636b05a7f79a63a4f7995b7e
evanjat2 <ann.jati2@gmail.com>
2023-05-30 12:52:39 +0700

    connect to azure api

---
+++ pages/test.js
@@ -0,0 +1,61 @@
+// <environment>
+"use strict";
+import { useEffect } from "react";
+
+import {
+  TextAnalysisClient,
+  AzureKeyCredential,
+} from "@azure/ai-text-analytics";
+
+export default function test() {
+  const documents = [
+    "I am very sad",
+     ];
+
+  async function main() {
+    const endpoint = "https://senprolangserv.cognitiveservices.azure.com/";
+    const apiKey = "9b6dd3495b8042088e552ab519275f1c";
+    const deploymentName = process.env.NEXT_PUBLIC_LANGSERV_DEPLOYMENTNAME;
+    const projectName = process.env.NEXT_PUBLIC_LANGSERV_PROJECTNAME;
+    const client = new TextAnalysisClient(
+      endpoint,
+      new AzureKeyCredential(apiKey)
+    );
+    const actions = [
+      {
+        kind: "CustomSingleLabelClassification",
+        deploymentName : deploymentName,
+        projectName: projectName,
+      },
+    ];
+    const poller = await client.beginAnalyzeBatch(actions, documents, "en");
+    const results = await poller.pollUntilDone();
+
+    for await (const actionResult of results) {
+      if (actionResult.kind !== "CustomSingleLabelClassification") {
+        throw new Error(
+          `Expected a CustomSingleLabelClassification results but got: ${actionResult.kind}`
+        );
+      }
+      if (actionResult.error) {
+        const { code, message } = actionResult.error;
+        throw new Error(`Unexpected error (${code}): ${message}`);
+      }
+      for (const result of actionResult.results) {
+        console.log(`- Document ${result.id}`);
+        if (result.error) {
+          const { code, message } = result.error;
+          throw new Error(`Unexpected error (${code}): ${message}`);
+        }
+        console.log(`\tClassification: ${result.classifications[0].category}`);
+      }
+    }
+  }
+
+  useEffect(() => {
+    main()
+  }, []);
+  return <div>test</div>;
+}
+
+// </main>
