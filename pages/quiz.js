38cefc6b010b8d51ffd8f1015075b35c6ac69437
evanjat2 <ann.jati2@gmail.com>
2023-06-04 23:08:41 +0700

    feat: get genre and movies after quiz

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,20 +1,23 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { useEffect, useState } from "react";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import {
   TextAnalysisClient,
   AzureKeyCredential,
 } from "@azure/ai-text-analytics";
+import { useContext } from "react";
+import { AppContext } from "../context/appContext";
+import loadingIcon from "../public/images/loading-icon.png"
 
 export default function quiz() {
   const router = useRouter();
-
+  const { getGenre } = useContext(AppContext);
   const [story, setStory] = useState([]);
   const [loading, setLoading] = useState(false);
   async function CustomSingleLabelClassification(story) {
     setLoading(true);
-    console.log(story);
     if (story.length == 0) {
       console.log("KOSONGG");
       return;
@@ -54,11 +57,16 @@ export default function quiz() {
           const { code, message } = result.error;
           throw new Error(`Unexpected error (${code}): ${message}`);
         }
-        console.log(`\tClassification: ${result.classifications[0].category}`);
+        const mood = result.classifications[0].category;
+        console.log(`\tClassification: ${mood}`);
+        localStorage.setItem("myMood", JSON.stringify(mood));
+        getGenre(mood);
       }
     }
     setLoading(false);
-    router.push("/");
+    router.push({
+      pathname: "/result",
+    });
   }
 
   const getEmotion = () => {
@@ -66,13 +74,8 @@ export default function quiz() {
   };
 
   useEffect(() => {
-    const documents = ["I am very sad"];
-    console.log(documents);
-    console.log(story);
-  }, [story]);
-
-  useEffect(() => {
     console.log(loading);
+    console.log(story);
   }, [loading]);
 
   return (
@@ -80,29 +83,36 @@ export default function quiz() {
       <div className="min-h-screen bg-gradient-to-b from-[#1E2A5E] to-[#000000]">
         <Header />
         <div className="text-center w-screen min-h-screen content-center grid font-poppins">
-          <p className="font-poppins text-2xl text-center text-white mb-8">
-            Share your thoughts and feelings
-          </p>
-          <p className="font-poppins text-base text-center text-white mb-8">
-            It helps to figure out what's going on with you right now!
-          </p>
-          <div className=" transition-all gap-4 grid text-left font-poppins bg-gradient-to-bl from-[#B999ED] to-[#5B9FD9] w-[55vw] min-h-[45vh] m-auto rounded-[45px] p-[22px] mb-8">
-            <div className="p-3 relative mb-3" data-te-input-wrapper-init>
-              <textarea
-                onChange={(event) => setStory(event.target.value)}
-                className="py-3 textarjustify-center peer block resize-none min-h-[auto] w-full rounded-[30px] outline-none border-0 bg-white px-3 leading-[1.6] transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-black dark:placeholder:text-grey dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-50"
-                id="exampleFormControlTextarea1"
-                rows="10"
-                placeholder="Write your feelings here..."
-              ></textarea>
-            </div>
-          </div>
-          <div
-            onClick={() => getEmotion()}
-            className="bg-white w-[55vw] bg-gradient-to-b from-[#E7E0C6] to-[#ACBCFF] m-auto rounded-[45px] p-2 font-bold cursor-pointer hover:scale-105 transition duration-500"
-          >
-            SUBMIT
-          </div>
+          {!loading && (
+            <>
+              <p className="font-poppins text-2xl text-center text-white mb-8">
+                Share your thoughts and feelings
+              </p>
+              <p className="font-poppins text-base text-center text-white mb-8">
+                It helps to figure out what's going on with you right now!
+              </p>
+              <div className=" transition-all gap-4 grid text-left font-poppins bg-gradient-to-bl from-[#B999ED] to-[#5B9FD9] w-[55vw] min-h-[45vh] m-auto rounded-[45px] p-[22px] mb-8">
+                <div className="p-3 relative mb-3" data-te-input-wrapper-init>
+                  <textarea
+                    onChange={(event) => setStory(event.target.value)}
+                    className="py-3 textarjustify-center peer block resize-none min-h-[auto] w-full rounded-[30px] outline-none border-0 bg-white px-3 leading-[1.6] transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-black dark:placeholder:text-grey dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-50"
+                    id="exampleFormControlTextarea1"
+                    rows="10"
+                    placeholder="Write your feelings here..."
+                  ></textarea>
+                </div>
+              </div>
+              <div
+                onClick={() => getEmotion()}
+                className="bg-white w-[55vw] bg-gradient-to-b from-[#E7E0C6] to-[#ACBCFF] m-auto rounded-[45px] p-2 font-bold cursor-pointer hover:scale-105 transition duration-500"
+              >
+                SUBMIT
+              </div>
+            </>
+          )}
+          {loading && (
+            <Image className="animate-spin m-auto" src={loadingIcon}/>
+          )}
         </div>
       </div>
       <Footer />
--- pages/result.js
+++ pages/result.js
@@ -1,35 +1,58 @@
-import Header from "../components/Header";
-import Image from "next/image";
-import gambar from "../public/images/result.png";
-export default function result() {
-  return (
-    <div className="bg-gradient-to-b from-[#1E2A5E] to-[#172048] min-h-screen text-white">
-      <Header />
-      <div className="font-poppins text-center px-[20%] pt-8 w-full">
-        <p className="pb-8 font-bold">
-          Congratulations! Here is your ideal movie pick
-        </p>
+import { useEffect, useContext, useState } from "react";
 
-        <div className="w-full">
-          <div className="flex gap-12 w-full">
-            <div className="w-[33%] shrink-0">
-              <Image src={gambar} className="w-full" />
-            </div>
-            <div className="text-justify">
-              <p className="pb-8 font-bold">Sit veniam quis et ea.</p>
-              <p>
-                Elit enim deserunt id culpa incididunt consequat velit.
-                Excepteur eu tempor ullamco enim esse laborum sunt occaecat
-                pariatur dolore exercitation do. Incididunt aute non minim eu
-                excepteur officia ut. Amet ipsum eiusmod proident enim non non
-                do eiusmod Lorem magna amet cillum pariatur amet.
-              </p>
-              <div></div>
-            </div>
-          </div>
-          <div></div>
+import Image from "next/image";
+import { useRouter } from "next/router";
+
+import axios from "axios";
+
+import { dataGenre } from "../data/dataGenre";
+import Header from "../components/Header";
+import gambar from "../public/images/result.png";
+import { AppContext } from "../context/appContext";
+
+export default function result() {
+  const [mood, setMood] = useState();
+  const [genre, setGenre] = useState();
+  const [movie, setMovie] = useState();
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setMood(JSON.parse(localStorage.getItem("myMood")));
+      setGenre(JSON.parse(localStorage.getItem("myGenre")));
+      setMovie(JSON.parse(localStorage.getItem("myMovie")));
+    }
+  }, []);
+  return (
+    <>
+      <div className="bg-gradient-to-b from-[#1E2A5E] to-[#172048] min-h-screen text-white">
+        <Header />
+        <div className="font-poppins text-center px-[20%] pt-8 w-full">
+          <p className="pb-8 font-bold">Your mood is {mood}</p>
+          <p className="pb-8 font-bold">
+            Congratulations! Here is your ideal movie pick with genre {genre}
+          </p>
+          {movie?.map((list) => (
+            <>
+              <div className="w-full pb-[5vh]" key={list._id}>
+                <div className="flex gap-12 w-full">
+                  <div className="w-[33%] shrink-0">
+                    <img
+                      alt="image-movie"
+                      src={list.primaryImage?.url}
+                      className="w-[80%]"
+                    ></img>
+                  </div>
+                  <div className="text-justify">
+                    <p className="pb-8 font-bold">{list.titleText.text}</p>
+                    <p>{list.plot.plotText.plainText}</p>
+                    <div></div>
+                  </div>
+                </div>
+                <div></div>
+              </div>
+            </>
+          ))}
         </div>
       </div>
-    </div>
+    </>
   );
 }
