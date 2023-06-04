11cd2e57c3513102a95cac9692d3f9450c0f7828
Rizky Intan Nurlita <79188089+rizkyintan@users.noreply.github.com>
2023-06-04 14:46:18 +0700

    finalisasi fitur search

--- pages/explore.js
+++ pages/explore.js
@@ -29,14 +29,14 @@ export default function ExplorePage() {
     };
 
     fetch(
-      "https://moviesdatabase.p.rapidapi.com/titles?list=top_rated_250&limit=20",
+      "https://moviesdatabase.p.rapidapi.com/titles?list=top_rated_250&limit=20&endYear=2023",
       options
     )
       .then((response) => response.json())
       .then((response) => setMovies(response.results))
       .catch((err) => console.error(err));
     fetch(
-      "https://moviesdatabase.p.rapidapi.com/titles?list=top_rated_series_250&limit=20",
+      "https://moviesdatabase.p.rapidapi.com/titles?list=top_rated_series_250&limit=20&endYear=2023",
       options
     )
       .then((response) => response.json())
@@ -45,13 +45,13 @@ export default function ExplorePage() {
     fetch(
       "https://moviesdatabase.p.rapidapi.com/titles/search/title/" +
         searchQuery +
-        "?exact=false&endYear=2023&limit=20",
+        "?exact=false&endYear=2023",
       options
     )
       .then((response) => response.json())
       .then((response) => setSearch(response.results))
       .catch((err) => console.error(err));
-  }, []);
+  }, [searchQuery]);
 
   useEffect(() => {
     console.log(movies);
@@ -74,24 +74,29 @@ export default function ExplorePage() {
 
           <div className="pl-[60px] ">
             <div className="text-xl font-bold mt-[40vh] mb-[20px] align-middle">
-              SEARCH RESULT FOR "{searchQuery}"
+              {search && search.length > 0
+                ? `SEARCH RESULTS FOR "${searchQuery}"`
+                : `NO SEARCH RESULTS FOUND FOR "${searchQuery}"`}
             </div>
+
             <div className="flex gap-12 overflow-x-scroll scrollbar-hide">
-              {search?.map((l) => (
-                <div key={l.id} className="flex-none w-[126px]">
-                  <div className="w-[126px] h-[200px]">
-                    <img
-                      alt="img-movie"
-                      className="w-full"
-                      src={l.primaryImage?.url}
-                    ></img>
+              {search
+                ?.filter((l) => l.primaryImage?.url)
+                .map((l) => (
+                  <div key={l.id} className="flex-none w-[126px]">
+                    <div className="w-[126px] h-[200px]">
+                      <img
+                        alt="img-movie"
+                        className="w-full"
+                        src={l.primaryImage?.url}
+                      ></img>
+                    </div>
+                    <div className="font-bold">{l.titleText.text}</div>
+                    <div className="font-bold text-[#AFAFAF] mb-[50px]">
+                      {l.releaseYear?.year}
+                    </div>
                   </div>
-                  <div className="font-bold">{l.titleText.text}</div>
-                  <div className="font-bold text-[#AFAFAF]">
-                    {l.releaseYear?.year}
-                  </div>
-                </div>
-              ))}
+                ))}
             </div>
           </div>
 
@@ -121,42 +126,50 @@ export default function ExplorePage() {
           <div className="bg-black fixed w-screen h-screen -z-30"></div>
 
           <div className="pl-[60px] ">
-            <div className="mt-[40vh]">MOVIES</div>
+            <div className="mt-[40vh] text-xl font-bold mb-3">
+              TOP MOVIES TODAY
+            </div>
             <div className="flex gap-12 overflow-x-scroll scrollbar-hide">
-              {movies?.map((l) => (
-                <div key={l.id} className="flex-none w-[126px]">
-                  <div className="w-[126px] h-[200px]">
-                    <img
-                      alt="img-movie"
-                      className="w-full"
-                      src={l.primaryImage?.url}
-                    ></img>
+              {movies
+                ?.filter((l) => l.primaryImage?.url)
+                .map((l) => (
+                  <div key={l.id} className="flex-none w-[126px]">
+                    <div className="w-[126px] h-[200px]">
+                      <img
+                        alt="img-movie"
+                        className="w-full"
+                        src={l.primaryImage?.url}
+                      ></img>
+                    </div>
+                    <div className="font-bold">{l.titleText.text}</div>
+                    <div className="font-bold text-[#AFAFAF]">
+                      {l.releaseYear?.year}
+                    </div>
                   </div>
-                  <div className="font-bold">{l.titleText.text}</div>
-                  <div className="font-bold text-[#AFAFAF]">
-                    {l.releaseYear?.year}
-                  </div>
-                </div>
-              ))}
+                ))}
             </div>
 
-            <div className="mt-[10vh]">SERIES</div>
+            <div className="mt-[10vh] text-xl font-bold mb-3">
+              TOP SERIES TODAY
+            </div>
             <div className="flex gap-12 overflow-x-scroll scrollbar-hide">
-              {series?.map((l) => (
-                <div key={l.id} className="flex-none w-[126px]">
-                  <div className="w-[126px] h-[200px]">
-                    <img
-                      alt="img-movie"
-                      className="w-full"
-                      src={l.primaryImage?.url}
-                    ></img>
+              {series
+                ?.filter((l) => l.primaryImage?.url)
+                .map((l) => (
+                  <div key={l.id} className="flex-none w-[126px]">
+                    <div className="w-[126px] h-[200px]">
+                      <img
+                        alt="img-movie"
+                        className="w-full"
+                        src={l.primaryImage?.url}
+                      ></img>
+                    </div>
+                    <div className="font-bold">{l.titleText.text}</div>
+                    <div className="font-bold text-[#AFAFAF]">
+                      {l.releaseYear?.year}
+                    </div>
                   </div>
-                  <div className="font-bold">{l.titleText.text}</div>
-                  <div className="font-bold text-[#AFAFAF]">
-                    {l.releaseYear?.year}
-                  </div>
-                </div>
-              ))}
+                ))}
             </div>
           </div>
 
