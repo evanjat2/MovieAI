d7d607851dd19a01bee88af8a189abbb89affbf2
evanjat2 <ann.jati2@gmail.com>
2023-06-06 00:23:12 +0700

    set favicon

---
+++ pages/_document.js
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+ 
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <link rel="icon" href="/images/favicon.png" />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
